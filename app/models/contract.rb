a1f1930fa1330ae1af308c22d5c9f95209ee683d
Gisilbek <gisilbek.parra@aicore.com>
2022-01-12 00:44:21 -0400

    Creando Controller Event

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,8 @@
 class Contract < ApplicationRecord
+  validates :name, presence: true
+  validates :week_start_time, presence: true
+  validates :week_end_time, presence: true
+  validates :weekend_start_time, presence: true
+  validates :weekend_end_time, presence: true
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :contract
+
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :personal, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+  enum status: { Ernesto: 1, Barbara: 2, Benjamin: 3 }
 end
